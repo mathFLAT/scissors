b817bbd13d9ec26cf2b5b51b6e00f95d55e365c4
Seokwon Jeong <sangcomz@naver.com>
2018-05-23 14:32:59 +0900

    remove blank

--- scissors-sample/src/main/java/com/lyft/android/scissorssample/MainActivity.java
+++ scissors-sample/src/main/java/com/lyft/android/scissorssample/MainActivity.java
@@ -87,8 +87,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
